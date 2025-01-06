aca3566707d8f34d0350ce703c6c5b5e427754f5
Ekekenta Clinton <60361247+icode247@users.noreply.github.com>
2025-01-06 23:16:27 +0100

    Update settings.py

--- multidoc/multidoc/settings.py
+++ multidoc/multidoc/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-$9e8+&q0q6-1=%aec(yp%3hh4=ihv5ht(=yg^im4+*p!j_ph1s')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
